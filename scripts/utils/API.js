ec2983c0b17258de309a87bd99135ef011a4a67d
Raphael Sanchez <hello@raphaelsanchez.design>
2023-11-15 01:59:12 +0100

    add api fetcher script

---
+++ scripts/utils/API.js
@@ -0,0 +1,54 @@
+// Create api to get data from json file
+export class API {
+  constructor() {
+    this._data = null
+  }
+
+  async fetchData() {
+    if (!this._data) {
+      const response = await fetch("../data/photographers.json")
+      this._data = await response.json()
+    }
+    return this._data
+  }
+}
+
+// Parse le JSON en toute sécurité
+function safeJSONParse(str, fallbackValue) {
+  try {
+    return JSON.parse(str)
+  } catch (error) {
+    return fallbackValue
+  }
+}
+
+// Fetch data from API or cache
+export async function getData() {
+  const cacheKey = "photographersData"
+  const cacheDuration = 60 * 60 * 1000 // 1 hour in milliseconds
+
+  // on retrouve les données "cachées"
+  const cachedData = safeJSONParse(localStorage.getItem(cacheKey), null)
+
+  // TODO: replace timestamp by etag
+  if (cachedData) {
+    const { data, timestamp } = cachedData
+    if (Date.now() - timestamp < cacheDuration) {
+      console.log("Serving from cache")
+      return data
+    }
+  }
+
+  try {
+    const api = new API()
+    const data = await api.fetchData()
+
+    // mise à jour du cache
+    const cacheData = { data, timestamp: Date.now() }
+    localStorage.setItem(cacheKey, JSON.stringify(cacheData))
+    return data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
